4dc1ea34d99fb1ccd030ade7d4e4d3888e98fb5a
Maria Pradeep <123819976+pradeepmaria@users.noreply.github.com>
2024-02-06 19:56:41 +0530

    Add files via upload

---
+++ 002.c
@@ -0,0 +1,23 @@
+/*
+Name:
+Date:
+Description:
+Sample Input:
+Sample Output:
+*/
+#include<stdio.h>
+int main()
+{
+	//	char *ptr = malloc(5);
+	//	const int *ptr;
+		int *const ptr;
+
+		ptr = ptr + ptr;
+		ptr = ptr - ptr;
+		ptr = ptr *ptr;
+		ptr = ptr / ptr;
+
+}
+				
+
+
