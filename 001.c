401a47fdf13277d6cc8ccfd8d5324544eb6c3968
Maria Pradeep <123819976+pradeepmaria@users.noreply.github.com>
2024-02-06 19:55:11 +0530

    Add files via upload

---
+++ 001.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+		int num1=10;
+		int num2=20;
+
+		printf("num1 is %d \n",num1++);
+		printf("num1 is %d \n",num1);
+				printf("num2 is %d\n",++num2);
+
+return 0;
+}
+
+
+
