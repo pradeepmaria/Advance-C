8034b9b9649ad2c7c52a4ba8728abaa79d7dd945
Maria Pradeep <123819976+pradeepmaria@users.noreply.github.com>
2024-02-07 14:19:32 +0530

    Add files via upload

---
+++ clear_lastnbits.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int num,n,i;
+	printf("enter the num and bit position n:\n");
+	scanf("%d%d",&num,&n);
+	 
+	i = (num &( ~ (1<<(n-1)))) ;
+
+	printf("%d\n",i);
+
+	return 0;
+}
