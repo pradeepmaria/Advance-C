170a895d952cbd115a5fa103a2bfa4cbe487f1b5
Maria Pradeep <123819976+pradeepmaria@users.noreply.github.com>
2024-02-07 14:33:50 +0530

    Add files via upload

---
+++ pattern10.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{
+	int i,j,rows;
+	printf("enter the no of rows\n");
+	scanf("%d",&rows);
+
+	for(i=1;i<=rows;i++)
+	{
+		for(j=1;j<=rows;j++)
+		{
+			if((i+j)<=rows)
+			{
+				printf(" ");
+			}
+			else 
+				printf("*");
+		}
+		printf("\n");
+	}
+	for(i=1;i<=rows;i++)
+	{
+		for(j=rows;j>=i;j--)
+		{
+			if((i+j)<=rows)
+			{
+				printf(" ");
+			}
+			else
+				printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
