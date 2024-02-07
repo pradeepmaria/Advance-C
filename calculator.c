54a38824acbb81228d12ee14d7379679c91b0ee0
Maria Pradeep <123819976+pradeepmaria@users.noreply.github.com>
2024-02-07 14:16:23 +0530

    Add files via upload

---
+++ calculator.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("enter two numbers:");
+	scanf("%d %d",&a,&b);
+	
+	printf("addition of a + b = %d\n",a + b);
+        printf("subtraction of a - b = %d\n",a - b);
+	printf("multiplication of a * b = %d\n",a * b);
+	printf("division of a / b = %d\n",a / b);
+}
