638dcdd5a8c4680abfcce96e04c407e62a535943
Maria Pradeep <123819976+pradeepmaria@users.noreply.github.com>
2024-02-07 14:12:27 +0530

    Add files via upload

---
+++ bitwiseand.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int num,num1,num2;
+	printf("enter the two number num1 & num2.\n");
+	scanf("%d %d",&num1,&num2);
+
+
+	num = num1 & num2;
+	printf("%d & %d = %d\n",num1,num2,num);
+
+	return 0;
+}
