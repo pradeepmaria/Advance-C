a2146ae4932b73c8bdbaba41dda4a22ae9241d57
Maria Pradeep <123819976+pradeepmaria@users.noreply.github.com>
2024-02-07 14:21:11 +0530

    Add files via upload

---
+++ descending_array.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+	int i,j,temp,size;
+	printf("enter the size of the array :");
+	scanf("%d",&size);
+	int arr[size];
+	printf("enter the array elements : ");
+	for(i=0;i<size;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+
+	printf("Descending order is : ");
+	for(i=0;i<size;i++)
+	{
+		for(j=i+1;j<size;j++)
+		{
+			if(arr[i] < arr[j])
+			{
+				temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+		}
+		printf("%d ",arr[i]);
+	}
+	printf("\n");
+}	
